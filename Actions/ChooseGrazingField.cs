37fc276d51011fe8653735def787711cbc6dd8b6
Kristen Norris <krnorris65@gmail.com>
2019-07-16 15:44:16 -0500

    user can add cow to a field and will not be able to add to field if field is at capacity. buggy but basic functionality done

--- Actions/ChooseGrazingField.cs
+++ Actions/ChooseGrazingField.cs
@@ -3,26 +3,34 @@ using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
+using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class ChooseGrazingField {
-        public static void CollectInput (Farm farm, IGrazing animal) {
+namespace Trestlebridge.Actions
+{
+    public class ChooseGrazingField
+    {
+        public static void CollectInput(Farm farm, IGrazing animal)
+        {
             Console.Clear();
 
             for (int i = 0; i < farm.GrazingFields.Count; i++)
             {
-                Console.WriteLine ($"{i + 1}. Grazing Field");
+                Console.WriteLine($"{i + 1}. Grazing Field");
             }
 
-            Console.WriteLine ();
+            Console.WriteLine();
 
             // How can I output the type of animal chosen here?
-            Console.WriteLine ($"Place the animal where?");
+            Console.WriteLine($"Place the animal where?");
 
-            Console.Write ("> ");
-            int choice = Int32.Parse(Console.ReadLine ());
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+            //index of list starts at 0, so the index will always be one less than the value the user selects
+            int choiceIndex = choice - 1;
 
-            farm.GrazingFields[choice].AddResource(animal);
+            GrazingField grazingField = farm.GrazingFields[choiceIndex];
+
+            AddAnimalToField(farm, grazingField, animal);
 
             /*
                 Couldn't get this to work. Can you?
@@ -30,6 +38,19 @@ namespace Trestlebridge.Actions {
              */
             // farm.PurchaseResource<IGrazing>(animal, choice);
 
+        }
+        public static void AddAnimalToField(Farm farm, GrazingField field, IGrazing animal){
+            if (field.FieldFull)
+            {
+                Console.WriteLine("This field is full");
+                Console.WriteLine("Press Enter to Select New field");
+                Console.ReadLine();
+                CollectInput(farm, animal);
+            }
+            else
+            {
+                field.AddResource(animal);
+            }
         }
     }
 }
--- Actions/PurchaseStock.cs
+++ Actions/PurchaseStock.cs
@@ -4,22 +4,37 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Pig");
+            Console.WriteLine("3. Goat");
+            Console.WriteLine("4. Ostrich");
+            Console.WriteLine("5. Sheep");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             switch (Int32.Parse(choice))
             {
                 case 1:
                     ChooseGrazingField.CollectInput(farm, new Cow());
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+
+                    break;
+                case 5:
                     break;
                 default:
                     break;
