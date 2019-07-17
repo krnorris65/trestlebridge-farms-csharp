d2ca46f06759e2a10e24d3448e30cb77671ddd9b
Kristen Norris <krnorris65@gmail.com>
2019-07-17 14:41:06 -0500

    fixed interface error

--- Models/Facilities/PlowedField.cs
+++ Models/Facilities/PlowedField.cs
@@ -24,7 +24,7 @@ namespace Trestlebridge.Models.Facilities
             }
         }
 
-        public double PlantCapacity
+        public double Capacity
         {
             get
             {
