d3bd88face26d880caf940938eadbd850f16df68
Kristen Norris <krnorris65@gmail.com>
2019-07-22 20:44:56 -0500

    refactored DuckHouse

--- Models/Facilities/ChickenHouse.cs
+++ Models/Facilities/ChickenHouse.cs
@@ -3,7 +3,7 @@ namespace Trestlebridge.Models.Facilities
 {
     public class ChickenHouse : Facility
     {
-        private int _capacity;
+        private int _capacity = 15;
         public override string Type {get; } = "Chicken House";
         public override double Capacity 
         {
